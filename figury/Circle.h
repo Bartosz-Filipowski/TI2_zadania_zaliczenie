6d38278953f6ec2bbf8c678dfe84fba8efb5244e
Bartosz-Filipowski <franek.filipowski1@gmail.com>
2025-06-18 12:53:56 +0200

    Create Circle.h

---
+++ figury/Circle.h
@@ -0,0 +1,18 @@
+#ifndef FIGURES_CIRCLE_H
+#define FIGURES_CIRCLE_H
+#include "Figure.h"
+class Circle : public Figure
+{
+private:
+    float m_r;
+    float m_area;
+    float m_perimeter;
+
+public:
+    Circle(string name = "", float r = 0);
+    virtual ~Circle() {}
+    virtual float Area() const;
+    virtual float Perimeter() const;
+    virtual void Info() const;
+};
+#endif //FIGURES_CIRCLE_H
