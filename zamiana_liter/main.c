5d8c6fb943ae763c49876c72534a30c2c1236bf7
Bartosz-Filipowski <franek.filipowski1@gmail.com>
2025-06-18 13:06:41 +0200

    Update main.c

--- zamiana_liter/main.c
+++ zamiana_liter/main.c
@@ -2,36 +2,74 @@
 
 void small_letters(char *tekst)
 {
-    while(*tekst)
-    {
-    if ('A' <= *tekst && *tekst <= 'Z')
-    {
-        *tekst += ('a' - 'A');
-    }
-    tekst++;
-    }
+    while(*tekst)
+    {
+    if ('A' <= *tekst && *tekst <= 'Z')
+    {
+        *tekst += ('a' - 'A');
+    }
+    tekst++;
+    }
 }
 void big_letters(char *tekst)
 {
-    while(*tekst)
-    {
-        if ('a' <= *tekst && *tekst <= 'z')
-        {
-            *tekst -= ('a' - 'A');
-        }
-        tekst++;
-    }
+    while(*tekst)
+    {
+        if ('a' <= *tekst && *tekst <= 'z')
+        {
+            *tekst -= ('a' - 'A');
+        }
+        tekst++;
+    }
+}
+int how_many_letters(char *tekst)
+{
+    int l = 0;
+    while(*tekst)
+    {
+        if (('A' <= *tekst && *tekst <= 'Z') || ('a' <= *tekst && *tekst <= 'z'))
+        {
+            l ++;
+        }
+        tekst++;
+
+    }
+return l;
+
+}
+int combinestr(char *source1, char *source2, char *source3) {
+    while (*source1) {
+        *source3 = *source1;
+        source1++;
+        source3++;
+    }
+
+
+    while (*source2) {
+        *source3 = *source2;
+        source2++;
+        source3++;
+    }
+    return 0;
 }
 
 
 
 
 int main() {
-    char tekst[] = "PrzYKladoWY.? nAPisss!!$";
-    printf("%s \n",tekst);
-    small_letters(tekst);
-    printf("%s\n", tekst);
-    big_letters(tekst);
-    printf("%s\n", tekst);
-    return 0;
+    char str1[]="Ala";
+    char str2[]=" ma kota";
+    char str3[80]="";
+    char tekst[] = "PrzYKladoWY.? nAPisss!!$";
+    printf("%s \n",tekst);
+    small_letters(tekst);
+    printf("%s\n", tekst);
+    big_letters(tekst);
+    printf("%s\n", tekst);
+    int ilosc = how_many_letters(tekst);
+    printf("%d\n", ilosc);
+
+    combinestr(str1, str2, str3);
+    printf("%s\n", str3);
+    return 0;
 }
