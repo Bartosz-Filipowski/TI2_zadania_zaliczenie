8f077699f4cc949adc6e9b6eb83370bc16e5d95d
Bartosz-Filipowski <franek.filipowski1@gmail.com>
2025-06-18 13:57:06 +0200

    Create list.h

---
+++ stosowa_lista/list.h
@@ -0,0 +1,30 @@
+
+#ifndef LIST_H
+#define LIST_H
+
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+
+
+int push(int element);
+
+
+int pop();
+
+
+void printStack(void);
+
+
+int getNodesCount();
+
+
+void freeStack(void);
+
+
+
+
+
+
+#endif //LIST_H
