a8ca935f05493edaa77856282db0a9f88e0abae9
Bartosz-Filipowski <franek.filipowski1@gmail.com>
2025-06-18 13:40:39 +0200

    Create list.h

---
+++ kolejkowa_lista/list.h
@@ -0,0 +1,24 @@
+#ifndef LIST_H
+#define LIST_H
+
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+int enqueue(int element);
+
+
+int dequeue();
+
+
+void printQueue(void);
+
+int getNodesCount();
+
+
+void freeQueue(void);
+
+
+
+
+#endif //LIST_H
