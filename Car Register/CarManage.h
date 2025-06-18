2e552755c14fff589c3835ef9458b92b2aa85751
Bartosz-Filipowski <franek.filipowski1@gmail.com>
2025-06-18 14:01:26 +0200

    Create CarManage.h

---
+++ Car Register/CarManage.h
@@ -0,0 +1,17 @@
+
+#ifndef CARMANAGE_H
+#define CARMANAGE_H
+
+
+
+#include "Car.h"
+
+void addCar();
+void showAllCars();
+void showCarById();
+void deleteCarById();
+void saveToFile();
+
+
+
+#endif //CARMANAGE_H
