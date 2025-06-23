d0600c3a01affb55588d574004381b23bda91e32
Bartosz-Filipowski <franek.filipowski1@gmail.com>
2025-06-23 09:55:21 +0200

    Create AutoFunckje.h

---
+++ Lista_Aut/AutoFunckje.h
@@ -0,0 +1,10 @@
+#ifndef AUTOFUNKCJE_H
+#define AUTOFUNKCJE_H
+#include "Auto.h"
+
+void dodajAuto();
+void pokazWszystkieAuta();
+void pokazAutoid();
+void usunAutoid();
+void zapiszDoPliku();
+#endif //AUTOFUNKCJE_H
